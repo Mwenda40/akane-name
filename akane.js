5ea337414044a924a7886e6f1d73b16ab74956e2
mwenda40 <iankirimi@yahoo.com>
2021-07-19 16:52:06 +0300

    add function in akane.js

--- akane.js
+++ akane.js
@@ -1,7 +1,20 @@
-var birthday = "07 / 07 / 2015";
-var arr1 = birthday.split(' ');
-var arr2 = arr1[1].split(',');
+function(){
+    var birthday = "11/01/2018";
+    var arr = birthday.split('/');
+    var day = arr[0];
+    var month = arr[1];
+    var split_day = day.split('');
+    var split_month = month.split('');
+    var dd = parseInt(split_day[0] + split_day[1]);
+    var mm = parseInt(split_month[0] + split_month[1]);
+    var year = arr[2];
+    var split_year = year.split('');
+    var cc = parseInt(split_year[0] + split_year[1]);
+    var yy = parseInt(split_year[2] + split_year[3]);
 
-console.log('date: ', arr1[0]);
-console.log('month: ', arr2[0]);
-console.log('year: ', arr2[1]);
+ 
+function maleAkane(){
+    //console.log((((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7);
+    console.log(( ( (cc/4) -2*cc-1) + ((5*(yy/4)) ) + ((26*(mm+1)/10)) + dd ) % 7);
+}
+maleAkane();
